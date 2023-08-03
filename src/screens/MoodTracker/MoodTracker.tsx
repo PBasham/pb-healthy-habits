0ad05c52fb5c1587f9fb44ebc481e782fdf53df2
patrickbasham <PatrickTBasham@gmail.com>
2023-08-02 22:42:33 -0400

    updated the styling and containers that will display the 'overall mood' and the 'daily entries for mood'

--- src/screens/MoodTracker/MoodTracker.tsx
+++ src/screens/MoodTracker/MoodTracker.tsx
@@ -30,14 +30,42 @@ const MoodTracker: FunctionComponent<MoodTrackerProps> = (props: MoodTrackerProp
 
     const [logMessage, setLogMessage] = useState<string>("")
 
+
+    //TODO - screen container
+    //TODO - overall mood container
+    //TODO - Today's entries contaienr
     const MoodTrackerContainer = styled(Container)`
         width: 100%;
         height: 100%;
         padding: 40px 20px;
-    `;
+        `;
 
-    const CreateLogContainer = styled(Container)`
+    //TODO - overall mood container
+    const OverallMoodContainer = styled(ContainerFlexTwo)`
+        background-color: purple;
+    `
+    const OverallMoodInnerContainer = styled(ContainerFlexTwo)`
         
+        justify-content: center;
+        align-items: center;
+
+        padding-top: 10px;
+        padding-bottom: 10px;
+
+        background-color: red;
+    `
+    const CreateLogContainer = styled(Container)`
+
+    `
+
+    //TODO - Today's entries contaienr
+    const DailyEntriesContainer = styled(ContainerFlexTwo)`
+        background-color: green;
+    `
+    const DailyEntriesInnerContainer = styled(ContainerFlexTwo)`
+        padding-top: 10px;
+        padding-bottom: 10px;
+        background-color: lime;
     `
     const CreateLogButtonContainer = styled(Container)`
         justify-content: center;
@@ -48,6 +76,9 @@ const MoodTracker: FunctionComponent<MoodTrackerProps> = (props: MoodTrackerProp
 
     async function handleGetMoodLog(): Promise<void> {
         const result = await logHelpers.getMoodLog()
+
+        console.log("Finished getting MoodLog")
+
         console.log("Result from handleGetMoodLog(): ", result.length)
         if (!result.length) {
             setMoodLog(result)
@@ -98,47 +129,88 @@ const MoodTracker: FunctionComponent<MoodTrackerProps> = (props: MoodTrackerProp
             <SafeAreaView edges={['top']} children={<TopBar label="Mood Tracker" />} />
             <SafeAreaView mode="padding" style={{ flex: 1 }} edges={['left', 'right']} >
                 <MoodTrackerContainer>
-                    <ContainerFlexTwo>
-                        <HeaderTwo text={"How are you feeling today overall?"} />
-                        <CreateLogButtonContainer>
+                    <OverallMoodContainer>
+
+                        <HeaderTwo
+                            text={"How are you feeling today?"}
+                            textColor={textColors.dark_transparent}
+                            textStyles={{
+                                marginTop: 10,
+                                marginBottom: 10,
+                            }}
+                        />
+                        <OverallMoodInnerContainer>
+                            {/* <CreateLogButtonContainer> */}
                             {!moodLog.length ?
+                                <StandardButton
+                                    text="Overall Mood"
+                                    iconName="add"
+                                    backgroundColor={generalColors.accent_blue}
+                                    // onPress={handleOpenMoodModal}//TODO set up OVERALL mood modal
+                                    textStyles={{ fontWeight: "bold" }}
+                                />
+                                // <HeaderThree
+                                //     text={logMessage}
+                                //     textStyles={{
+                                //         marginBottom: 10,
+                                //         width: "90%",
+                                //         color: textColors.dark_transparent
+                                //     }}
+                                // />
+                                : 
+                                <HeaderTwo
+                                    text="This will display the overall mood that was selected as well as have the option to modify it."
+                                />
+                            }
+                            {/* </CreateLogButtonContainer> */}
+                        </OverallMoodInnerContainer>
+                    </OverallMoodContainer>
+                    <DailyEntriesContainer>
+                        <HeaderTwo
+                            text="Today's entries"
+                            textAlignment="left"
+                            textColor={textColors.dark_transparent}
+                            textStyles={{
+                                marginTop: 10,
+                                marginBottom: 10,
+                            }}
+                        />
+                        {moodLog.length ?
+                            <DailyEntriesInnerContainer>
+                                <HeaderTwo
+                                    text="logs logs logs logs"
+                                />
+                            </DailyEntriesInnerContainer>
+
+                            :
+                            <>
+                                {/* //todo this needs to be a flatlist maybe? */}
+
                                 <HeaderThree
-                                    text={logMessage}
+                                    text="Nothing in log"
                                     textStyles={{
                                         marginBottom: 10,
                                         width: "90%",
+                                        flex: 1,
+                                        backgroundColor: "pink",
                                         color: textColors.dark_transparent
                                     }}
                                 />
-                                : null
-                            }
-                            <StandardButton
-                                text="Log overall day"
-                                iconName="add"
-                                backgroundColor={generalColors.accent_blue}
-                                // onPress={handleOpenMoodModal}
-                                textStyles={{ fontWeight: "bold" }}
-                            />
-                        </CreateLogButtonContainer>
-                    </ContainerFlexTwo>
+                            </>
+                        }
+                    </DailyEntriesContainer>
 
-                    {moodLog.length ?
-                        <ContainerFlexTwo>
-                            <HeaderTwo text="Today's log" />
-                        </ContainerFlexTwo>
-
-                        :
-                        // null
-                        <HeaderThree  text="Nothing in log"/>
-                    }
                     <StandardButton
-                        text={`${moodLog.length ? "" : "Log mood"}`}
+                        // text={`${moodLog.length ? "" : "Log mood"}`}
                         iconName="add"
                         backgroundColor={generalColors.accent_blue}
                         onPress={handleOpenMoodModal}
                         buttonStyles={{
-                                // alignSelf: "flex-end", 
-                            }}
+                            position: "absolute",
+                            borderRadius: 50,
+                            bottom: 20,
+                            right: 20
+                        }}
                         textStyles={{ fontWeight: "bold" }}
                     />
                 </MoodTrackerContainer>
